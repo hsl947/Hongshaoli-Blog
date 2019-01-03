d7fa6425c68ac5bd76edfe82879ae3a9a20208bb
hsl947 <582631730@qq.com>
2019-01-03 21:14:32 +0800

    c

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var routes = require('./routes/index');
 var app = express();
 app.use(favicon(path.join(__dirname, 'dist', 'favicon.ico')));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
 
@@ -19,13 +19,13 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/', routes);
 
 // 允许跨域访问
-app.all('*', function (req, res, next) {
+app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'x-Request-with')
     res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
     res.header('X-Powered-By', '4.15.2')
     res.header('Content-Type', 'application/json;charset=utf-8')
-    next()   //执行下一个中间件。
+    next() //执行下一个中间件。
 })
 
 app.listen(3030, (req, res) => {
